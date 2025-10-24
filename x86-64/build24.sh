098d532a7c40366854750a0a3108ad4c88221c8d
foxanddog-xx <81680025+foxanddog-xx@users.noreply.github.com>
2025-10-24 14:08:18 +0800

    Add UPNP and its localization packages
    Add UPNP packages to the build script.

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -63,6 +63,9 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# 添加UPNP
+PACKAGES="PACKAGES luci-app-upnp"
+PACKAGES="PACKAGES luci-i18n-upnp-zh-cn
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
